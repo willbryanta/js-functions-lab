1e7766b6ff00712096fe00c72b95fbd0b361a5d6
willbryant <bryanttwork@gmail.com>
2024-06-11 21:20:03 +1000

    Extra - Exercise 3

--- extra.js
+++ extra.js
@@ -13,3 +13,9 @@ function planetHasWater(planet){
 function computeArea(width, height){
     return `The area of a rectangle with a width of ${width} and a height of ${height} is ${width*height} square units.`
 }
+
+// Exercise 3
+
+function whoami(name, birthYear, hobby1, hobby2){
+    return `Hi, my name is ${name}. I am ${birthYear - 2024} years old. My hobbies are ${hobby1} and ${hobby2}.`
+}
