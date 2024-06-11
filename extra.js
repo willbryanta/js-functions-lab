fcf2eb89542f5133a37fef70f45f5ea653d61269
willbryant <bryanttwork@gmail.com>
2024-06-11 21:17:34 +1000

    Extra - Exercise 2

--- extra.js
+++ extra.js
@@ -8,3 +8,8 @@ function planetHasWater(planet){
     }
 }
 
+// Exercise 2
+
+function computeArea(width, height){
+    return `The area of a rectangle with a width of ${width} and a height of ${height} is ${width*height} square units.`
+}
