ce5e0689c29644744f1b1e31bf39385fa4074453
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2024-06-13 19:05:14 +1000

    Completed Exercise 12

--- script.js
+++ script.js
@@ -271,4 +271,8 @@ This function should return the count of arguments passed to it when called.
 Complete the exercise in the space below:
 */
 
+function numArgs(){
+    return arguments.length
+}
+
 console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
