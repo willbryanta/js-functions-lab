71e8af28cb0a951e678cfd7137a5b458a9cf2706
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2024-06-13 18:42:19 +1000

    Completed Exercise 10

--- script.js
+++ script.js
@@ -220,6 +220,21 @@ Example: calculateGrade(100) should return A.
 Complete the exercise in the space below:
 */
 
+function calculateGrade(score){
+    
+    if(score >= 90){
+        return 'A'
+    } else if (score < 90 && score >= 80){
+        return 'B'
+    } else if (score < 80 && score >= 70){
+        return 'C'
+    } else if (score < 70 && score >= 60){
+        return 'D'
+    } else if (score < 60){
+        return 'F'
+    }
+}
+
 console.log('Exercise 10 Result:', calculateGrade(85));
 
 
